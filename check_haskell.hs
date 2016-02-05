27ff8a18236f06e585fc910e646e9074ae453cde
Glenn G. Chappell <ggchappell@alaska.edu>
2016-02-04 19:54:52 -0900

    check_haskell.hs: fix operator problem that makes trouble on some systems

--- check_haskell.hs
+++ check_haskell.hs
@@ -1,6 +1,7 @@
 -- check_haskell.hs
 -- Glenn G. Chappell
 -- 3 Feb 2016
+-- Updated: 4 Feb 2016
 --
 -- For CS 331 Spring 2016
 -- A Haskell Program to Run
@@ -24,7 +25,7 @@ secret_message = map ordToC ords where
     d2 = [65,34,13,-4,25,-73,75,27,-3,-59]
     d3 = [61,40,14,-6,6,22,6,20,-1,-62]
     d4 = [57,46,10,0,7,22,-7,16,8]
-    diffs = map (+ -10) $ concat[d1, d2, d3, d4]
+    diffs = map (+ (-10)) $ concat[d1, d2, d3, d4]
     thisAndAdd x xs = x : map (+ x) xs
     ords = foldr thisAndAdd [] diffs
     ordToC n = toEnum n `asTypeOf` '@'
