4f6648976ed5cf76a46b2d0ad7e5a6dc34940c46
Glenn G. Chappell <ggchappell@alaska.edu>
2016-03-30 12:41:09 -0800

    Add Zebu interpretation code: interpit.lua (UNFINISHED), zebu.lua

---
+++ interpit.lua
@@ -0,0 +1,97 @@
+-- interpit.lua (UNFINISHED)
+-- Glenn G. Chappell
+-- 30 Mar 2016
+--
+-- For CS 331 Spring 2016
+-- Interpret AST from parseit.parse
+-- For Assignment 6, Exercise A
+
+
+local interpit = {}  -- Our module
+
+
+-- ***** Variables *****
+
+
+-- Symbolic Constants for AST
+
+local STMT_LIST  = 1
+local SET_STMT   = 2
+local PRINT_STMT = 3
+local NL_STMT    = 4
+local INPUT_STMT = 5
+local IF_STMT    = 6
+local WHILE_STMT = 7
+local BIN_OP     = 8
+local UN_OP      = 9
+local NUMLIT_VAL = 10
+local STRLIT_VAL = 11
+local ID_VAL     = 12
+local ARRAY_REF  = 13
+
+
+-- ***** Utility Functions *****
+
+
+-- toInt
+-- Given a number, return the number rounded toward zero.
+function toInt(n)
+    if n >= 0 then
+        return math.floor(n)
+    else
+        return math.ceil(n)
+    end
+end
+
+
+-- strToNum
+-- Given a string, attempt to interpret it as an integer. If this
+-- succeeds, return the integer. Otherwise, return 0.
+function strToNum(s)
+    -- Try to do string -> number conversion; make protected call
+    -- (pcall), so we can handle errors.
+    local success, value = pcall(function() return 0+s end)
+
+    -- Return integer value, or 0 on error.
+    if success then
+        return toInt(value)
+    else
+        return 0
+    end
+end
+
+
+-- numToStr
+-- Given a number, return its string form.
+function numToStr(n)
+    return ""..n
+end
+
+
+-- ***** Primary Function for Client Code *****
+
+
+-- interp
+-- Interpreter, given AST returned by parseit.parse.
+-- Parameters:
+--   ast     - AST constructed by parseit.parse
+--   state   - Table holding values of Zebu integer variables
+--             Value of simple variable xyz is in state.s["xyz"]
+--             Value of array item xyz[42] is in state.a["xyz"][42]
+--   incall  - Function to call for line input
+--             incall() inputs line, returns string with no newline
+--   outcall - Function to call for string output
+--             outcall(str) outputs str with no added newline
+--             To print a newline, do outcall("\n")
+-- Return Value:
+--   state updated with changed variable values
+function interpit.interp(ast, state, incall, outcall)
+    -- WRITE THIS!!!
+end
+
+
+-- ***** Module Export *****
+
+
+return interpit
+
---
+++ zebu.lua
@@ -0,0 +1,146 @@
+#!/usr/bin/env lua
+-- zebu.lua
+-- Glenn G. Chappell
+-- 30 Mar 2016
+--
+-- For CS 331 Spring 2016
+-- Interpreter for Zebu Programming Language
+-- Requires lexit.lua, parseit.lua, interpit.lua
+
+
+parseit = require "parseit"
+interpit = require "interpit"
+
+
+-- ***** Variables *****
+
+
+zebustate = { s={}, a={} }  -- Zebu state: variable values
+
+
+-- ***** Utility Functions *****
+
+
+-- inputLine
+-- Input a line of text from standard input and return it in string
+-- form, with no trailing newline.
+function inputLine()
+    return io.read("*l")
+end
+
+
+-- outputString
+-- Output the given string to standard output, with no added newline.
+function outputString(s)
+    io.write(s)
+end
+
+
+-- ***** Functions for Zebu Interpreter *****
+
+
+-- runZebu
+-- Given a string, attempt to treat it as source code for a Zebu
+-- program, and execute it. I/O uses standard input & output.
+-- Parameters:
+--   program  - Zebu source code
+--   state    - Values of Zebu variables as in interpit.interp.
+-- Returns two values:
+--   success  - true if program parsed successfully, false otherwise.
+--   newstate - If success is true, then new value of state, updated
+--              with revised values of variables. If success is false,
+--              then same as passed value of state.
+function runZebu(program, state)
+    local success, ast = parseit.parse(program)
+    local newstate
+    if success then
+        newstate = interpit.interp(ast, state, inputLine, outputString)
+    else
+        outputString("*** ERROR: Syntax error in Zebu source\n")
+        newstate = state
+    end
+    return success, newstate
+end
+
+
+-- isZebuSourceFilename
+-- Given string, return true if it looks like the name of a Zebu source
+-- file: no whitespace, ends with ".zebu", and has something before the
+-- ".". Otherwise, return false.
+function isZebuSourceFilename(s)
+    if s:len() < 5 then
+        return false
+    end
+    for i = 1, s:len() do
+        local c = s:sub(i,i)
+        if c == " " or c == "\t" or c == "\n" or c == "\r"
+          or c == "\f" then
+            return false
+        end
+    end
+    return s:sub(s:len()-4,s:len()) == ".zebu"
+end
+
+
+-- runFile
+-- Given filename, attempt to read source for a Zebu program from it,
+-- and execute the program.
+function runFile(fname)
+    function readable(fname)
+        local f = io.open(fname, "r")
+        if f ~= nil then
+            f:close()
+            return true
+        else
+            return false
+        end
+    end
+
+    if not readable(fname) then
+        io.write("*** ERROR: Zebu source file not readable\n")
+        return
+    end
+    local source = ""
+    for line in io.lines(fname) do
+        source = source .. line .. "\n"
+    end
+    zebustate = runZebu(source, zebustate)
+end
+
+
+-- repl
+-- Zebu REPL. Prompt & get a line. If it is blank, then exit. If it
+-- looks like the filename of a Zebu source file, then get Zebu source
+-- from it, execute, and exit. Otherwise, treat line as Zebu program,
+-- execute it, and REPEAT.
+function repl()
+    io.write("Type Zebu source filename (---.zebu) or Zebu program\n")
+    io.write("Blank line to exit\n")
+    while true do
+        io.write("> ")
+        local line = io.read("*l")  -- Read a line
+        if line == "" then
+            break
+        elseif (isZebuSourceFilename(line)) then
+            runFile(line)
+            break
+        else
+            zebustate = runZebu(line, zebustate)
+        end
+        io.write("\n")
+    end
+end
+
+
+-- ***** Main Program *****
+
+
+-- Command-line argument? If so treat as Zebu source filename, read
+-- source, and execute.
+if arg[1] ~= nil then
+    runFile(arg[1])
+-- Otherwise, fire up the Zebu REPL.
+else
+    repl()
+end
+
